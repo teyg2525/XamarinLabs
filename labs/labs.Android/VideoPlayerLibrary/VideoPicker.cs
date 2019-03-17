325defe196d2dee5468daabb18eb468018cf549f
teyg2525 <30502234+teyg2525@users.noreply.github.com>
2019-03-17 15:36:31 +0200

    Add files via upload

---
+++ labs/labs.Android/VideoPlayerLibrary/VideoPicker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using labs.Controls.Interfaces;
+using System.Threading.Tasks;
+
+[assembly: Dependency(typeof(labs.Droid.VideoPlayerLibrary.VideoPicker))]
+namespace labs.Droid.VideoPlayerLibrary
+{
+    class VideoPicker : IVideoPicker
+    {
+        public Task<string> GetVideoFileAsync()
+        {
+            Intent intent = new Intent();
+            intent.SetType("video/*");
+            intent.SetAction(Intent.ActionGetContent);
+            
+            MainActivity activity = MainActivity.Current;
+            
+            activity.StartActivityForResult(
+                Intent.CreateChooser(intent, "Select Video"),
+                MainActivity.PickImageId);
+            
+            activity.PickImageTaskCompletionSource = new TaskCompletionSource<string>();
+            
+            return activity.PickImageTaskCompletionSource.Task;
+        }
+    }
+}
