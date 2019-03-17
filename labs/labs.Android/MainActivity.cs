edbb7b2b5ab42e41e68caf967cadb070ae1cf232
teyg2525 <30502234+teyg2525@users.noreply.github.com>
2019-03-17 15:41:18 +0200

    Add files via upload

--- labs/labs.Android/MainActivity.cs
+++ labs/labs.Android/MainActivity.cs
@@ -1,16 +1,30 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 using Prism;
 using Prism.Ioc;
+using System.Threading.Tasks;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
 
 namespace labs.Droid
 {
     [Activity(Label = "labs", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public static MainActivity Current
+        {
+            private set; get;
+        }
+
+        public static readonly int PickImageId = 1000;
+
+        public TaskCompletionSource<string> PickImageTaskCompletionSource { set; get; }
         protected override void OnCreate(Bundle bundle)
         {
+            Current = this;
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
@@ -18,6 +32,23 @@ namespace labs.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App(new AndroidInitializer()));
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            if (requestCode == PickImageId)
+            {
+                if ((resultCode == Result.Ok) && (data != null))
+                {
+                    PickImageTaskCompletionSource.SetResult(data.DataString);
+                }
+                else
+                {
+                    PickImageTaskCompletionSource.SetResult(null);
+                }
+            }
         }
     }
 
@@ -28,5 +59,6 @@ namespace labs.Droid
             // Register any platform specific implementations
         }
     }
+
 }
 
