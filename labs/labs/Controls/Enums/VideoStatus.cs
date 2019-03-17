d8c99743a035dff20d58147be8d86f657e66a0b2
teyg2525 <30502234+teyg2525@users.noreply.github.com>
2019-03-17 15:26:07 +0200

    Add files via upload

---
+++ labs/labs/Controls/Enums/VideoStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace labs.Controls.Enums
+{
+    public enum VideoStatus
+    {
+        NotReady,
+        Playing,
+        Paused
+    }
+}
